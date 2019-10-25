174569a24e17100241f9f23d639fa8361ce5d685
lwl_lwl <979510482@qq.com>
2019-10-25 21:40:42 +0800

    登录模块1

---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+import Login from '@/views/login'
+Vue.use(VueRouter)
+export default new VueRouter({
+  routes: [{
+    path: '/login',
+    component: Login
+  }]
+})
